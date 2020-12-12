9d66cbda4da0908f44c90480da24754bcf76bcc1
tobols <tob.ols@hotmail.com>
2020-12-12 15:54:59 +0100

    11

--- AdventCode11/Program.cs
+++ AdventCode11/Program.cs
@@ -12,48 +12,94 @@ namespace AdventCode11
         static void Main(string[] args)
         {
             seats = ReadFile().ToArray();
-            last = (char[][])seats.Clone();
+            last = DeepClone(seats);
+            var iterations = 0;
 
-            for (int i = 0; i < seats.Length; i++)
-            {
-                for (int j = 0; j < seats[0].Length; j++)
-                {
-                    if (seats[i][j] == 'L' || seats[i][j] == '#')
-                        EvaluateSeat(i, j);
-                }
-            }
+            while (UpdateSeats()) { Console.WriteLine($"Iterations: {++iterations}"); }
+            Console.WriteLine($"Occupied seats: {CountOccupied()}");
         }
 
+        private static bool UpdateSeats()
+        {
+            var hasChanges = false;
 
-        private static void EvaluateSeat(int x, int y)
+            for (int i = 0; i < seats.Length; i++)
+                for (int j = 0; j < seats[0].Length; j++)
+                    if (seats[i][j] == 'L' || seats[i][j] == '#')
+                        hasChanges = EvaluateSeat(i, j) || hasChanges;
+
+            if (hasChanges)
+                last = DeepClone(seats);
+
+            return hasChanges;
+        }
+
+        private static bool EvaluateSeat(int x, int y)
         {
             var counter = 0;
-
-            for (int i = (x - 1) >= 0 ? (x-1) : 0; i <= x + 1 && i < seats.Length; i++)
+            for (int i = -1; i <= 1; i++)
             {
-                for (int j = (y - 1) >= 0 ? (y-1): 0; j <= y + 1 && j < seats[0].Length; j++)
+                for (int j = -1; j <= 1; j++)
                 {
-                    if (i == x && j == y)
+                    if (i == 0 && j == 0)
                         continue;
 
-                    if (seats[x][y] == 'L' && last[i][j] == '#')
+                    if (seats[x][y] == 'L' && Look(x, y, i, j, seats.Length))
                         counter++;
-                    else if (last[i][j] == '#')
+                    else if (seats[x][y] == '#' && Look(x, y, i, j, seats.Length))
                         counter++;
                 }
             }
 
             if (seats[x][y] == 'L' && counter == 0)
                 seats[x][y] = '#';
-
-            else if (seats[x][y] == '#' && counter >= 4)
+            else if (seats[x][y] == '#' && counter >= 5)
                 seats[x][y] = 'L';
+            else
+                return false;
+
+            return true;
+        }
+
+        private static bool Look(int x, int y, int dx, int dy, int range = 1)
+        {
+            var c = 1;
+
+            while(x + dx * c >= 0 && y + dy * c >= 0 && x + dx * c < seats.Length && y + dy * c < seats[0].Length && c <= range)
+            {
+                if (last[x + dx * c][y + dy * c] == '#') return true;
+                if (last[x + dx * c][y + dy * c] == 'L') return false;
+                c++;
+            }
+
+            return false;
+        }
+
+        private static char[][] DeepClone(char[][] arr)
+        {
+            char[][] copyArr = new char[arr.Length][];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                copyArr[i] = new char[arr[i].Length];
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    copyArr[i][j] = arr[i][j];
+                }
+            }
+
+            return copyArr;
+        }
+
+        private static int CountOccupied()
+        {
+            return seats.SelectMany(s => s).Where(c => c == '#').Count();
         }
 
 
         private static IEnumerable<char[]> ReadFile()
         {
-            var file = new System.IO.StreamReader(@"C:\Repos\temp\AdventCode\AdventCode11\data.txt");
+            var file = new System.IO.StreamReader(@"D:\Repos\temp\adventcode20\AdventCode11\data.txt");
             string line;
             while ((line = file.ReadLine()) != null)
                 yield return line.ToCharArray();
